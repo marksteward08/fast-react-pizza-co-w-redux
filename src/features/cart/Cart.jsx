742e7aa30ca7470fde83e49304d3768ac4334224
Mark Steward Bayot <marksteward08@gmail.com>
2025-09-06 00:49:49 +0800

    Enhance Cart component with clear cart functionality and add EmptyCart display for empty state

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -3,7 +3,9 @@ import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
 import CartItem from './CartItem';
 import { useSelector } from 'react-redux';
-import { getCart } from './cartSlice';
+import { getCart, clearCart, getTotalCartItems } from './cartSlice';
+import { useDispatch } from 'react-redux';
+import EmptyCart from './EmptyCart';
 
 const fakeCart = [
   {
@@ -31,27 +33,46 @@ const fakeCart = [
 
 function Cart() {
   const username = useSelector((state) => state.user.username);
+  const totalCartItems = useSelector(getTotalCartItems);
   const cart = useSelector(getCart);
+  const dispatch = useDispatch();
+
+  function handleClearCart() {
+    console.log('clear cart');
+
+    dispatch(clearCart());
+  }
 
   return (
     <div className="px-4 py-3">
-      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
+      {totalCartItems === 0 ? (
+        <EmptyCart />
+      ) : (
+        <>
+          <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2 className="mt-7 text-xl font-semibold">Your cart, {username}</h2>
+          <h2 className="mt-7 text-xl font-semibold">Your cart, {username}</h2>
 
-      <ul className="mt-3 divide-y divide-stone-200 border-b">
-        {cart.map((item) => (
-          <CartItem item={item} key={item.key} />
-        ))}
-      </ul>
+          <ul className="mt-3 divide-y divide-stone-200 border-b">
+            {cart.map((item) => (
+              <CartItem item={item} key={item.id} />
+            ))}
+          </ul>
 
-      <div className="mt-6 space-x-2">
-        <Button to="/order/new" type="primary">
-          Order pizzas
-        </Button>
-
-        <Button type="secondary">Clear cart</Button>
-      </div>
+          <div className="mt-6 space-x-2">
+            {totalCartItems > 0 && (
+              <>
+                <Button to="/order/new" type="primary">
+                  Order pizzas
+                </Button>
+                <Button onClick={handleClearCart} type="secondary">
+                  Clear cart
+                </Button>
+              </>
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/features/cart/EmptyCart.jsx
+++ src/features/cart/EmptyCart.jsx
@@ -3,9 +3,13 @@ import { Link } from 'react-router-dom';
 function EmptyCart() {
   return (
     <div>
-      <Link to="/menu">&larr; Back to menu</Link>
+      <Link className="text-blue-800" to="/menu">
+        &larr; Back to menu
+      </Link>
 
-      <p>Your cart is still empty. Start adding some pizzas :)</p>
+      <p className="font-bold text-gray-800">
+        Your cart is still empty. Start adding some pizzas :)
+      </p>
     </div>
   );
 }
