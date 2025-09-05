b9e7aea9f33f165ee05ada1a5526226f6824541c
Mark Steward Bayot <marksteward08@gmail.com>
2025-09-05 22:39:05 +0800

    Add conditional rendering for username prompt in Username component

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -2,6 +2,9 @@ import { useSelector } from 'react-redux';
 
 function Username() {
   const username = useSelector((state) => state.user.username);
+
+  if (!username) return <div>Enter your username</div>;
+
   return (
     <div className="hidden text-sm font-semibold md:block">{username}</div>
   );
