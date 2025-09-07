e29c8b6d4ae20565e6413975c4d104fe74793b76
Mark Steward Bayot <marksteward08@gmail.com>
2025-09-07 18:57:26 +0800

    Add address fetching functionality: implement fetchAddress async thunk and integrate geolocation in CreateOrder component

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getCart, clearCart, getTotalPrice } from '../cart/cartSlice';
 import EmptyCart from '../cart/EmptyCart';
 import store from '../../store';
+import { fetchAddress } from '../user/userSlice';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -17,6 +18,7 @@ function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
   const username = useSelector((state) => state.user.username);
+  const address = useSelector((state) => state.user.address);
 
   const formErrors = useActionData();
 
@@ -26,11 +28,15 @@ function CreateOrder() {
   const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
   const totalPrice = totalCartPrice + priorityPrice;
 
+  const dispatch = useDispatch();
+
   if (cart.length === 0) return <EmptyCart />;
 
   return (
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
+
+      <button onClick={() => dispatch(fetchAddress())}>Get Position</button>
 
       {/* <Form method="POST" action="/order/new"> */}
       <Form method="POST">
@@ -64,6 +70,7 @@ function CreateOrder() {
               className="input w-full"
               type="text"
               name="address"
+              defaultValue={address}
               required
             />
           </div>
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,6 +1,31 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { getAddress } from '../../services/apiGeocoding';
+
+async function getPosition() {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+}
+
+export const fetchAddress = createAsyncThunk('user/fetchAddress', async () => {
+  const currentPosition = await getPosition();
+  const position = {
+    latitude: currentPosition.coords.latitude,
+    longitude: currentPosition.coords.longitude,
+  };
+
+  const addressObj = await getAddress(position);
+  const address = `${addressObj.principalSubdivision} ${addressObj.city}, ${addressObj.postcode}, ${addressObj.countryName}`;
+
+  // payload of the fulfilled state
+  return { position, address };
+});
+
 const initialState = {
   username: '',
+  status: 'idle',
+  position: {},
+  address: '',
 };
 
 const userSlice = createSlice({
@@ -11,6 +36,20 @@ const userSlice = createSlice({
       state.username = action.payload;
     },
   },
+  extraReducers: (builder) =>
+    builder
+      .addCase(fetchAddress.pending, (state, action) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchAddress.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.position = action.payload.position;
+        state.address = action.payload.address;
+      })
+      .addCase(fetchAddress.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      }),
 });
 
 export const { updateUsername } = userSlice.actions;
