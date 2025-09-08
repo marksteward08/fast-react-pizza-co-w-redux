c721f7558c542553c25db42adb68e9603165be93
Mark Steward Bayot <marksteward08@gmail.com>
2025-09-08 11:21:23 +0800

    Add fetcher to load data from menu route (WIP)

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -2,16 +2,24 @@
 
 import OrderItem from './OrderItem';
 
-import { useLoaderData } from 'react-router-dom';
+import { useFetcher, useLoaderData } from 'react-router-dom';
 import { getOrder } from '../../services/apiRestaurant';
 import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
 } from '../../utils/helpers';
+import { useEffect } from 'react';
 
 function Order() {
   const order = useLoaderData();
+  const fetcher = useFetcher();
+
+  useEffect(() => {
+    if (!fetcher.data && fetcher.state === 'idle') fetcher.load(`/menu`);
+  }, [fetcher]);
+
+  console.log(fetcher);
 
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
